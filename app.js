d7584fc6d52b97cac87dcb4aa3476dac07df8f5f
Kyosuke INOUE <kyoro@hakamastyle.net>
2011-06-29 09:31:42 +0900

    ccccccccc

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var fcgiApp = require("./fcgi"),
 var myServer = http.createServer(function(req, res) {
 	setTimeout(function() {
 		res.writeHead(200, {"Content-type": "text/html"});
-		res.end("It Miko Moe Works 554444444444o3!" + Date.now());
+		res.end("It Miko Moe Works uuuuuu554444444444o3!" + Date.now());
 		console.log("Wrote response.");
 	}, 1000);
 	//throw new Error("Bollocks.");
